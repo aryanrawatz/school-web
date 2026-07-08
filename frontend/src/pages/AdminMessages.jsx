ab7b46cad4a7efdc56a5d23415b07ac6f571806b
aryanrawatz <beepoye@gmail.com>
2026-07-08 22:01:20 +0530

    Fix admin messages page

--- frontend/src/pages/AdminMessages.jsx
+++ frontend/src/pages/AdminMessages.jsx
@@ -1,8 +1,8 @@
 function AdminMessages() {
   return (
-    <h1 style={{ padding: "50px", color: "red" }}>
-      ADMIN PAGE WORKING
-    </h1>
+    <div style={{ padding: "50px" }}>
+      <h1>ADMIN PAGE WORKING ✅</h1>
+    </div>
   );
 }
 
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,23 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home/Home";
-import About from "../pages/About/About";
+
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Admission from "../pages/Admission";
+import Gallery from "../pages/Gallery";
+import Academics from "../pages/Academics";
+import AdminMessages from "../pages/AdminMessages";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/admission" element={<Admission />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/academics" element={<Academics />} />
+      <Route path="/admin-messages" element={<AdminMessages />} />
     </Routes>
   );
 }
