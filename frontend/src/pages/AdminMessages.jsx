646c69495db086dc184f33a5a6c7fd6d8938f979
aryanrawatz <beepoye@gmail.com>
2026-07-09 22:17:43 +0530

    Test Admin Page

--- frontend/src/pages/AdminMessages.jsx
+++ frontend/src/pages/AdminMessages.jsx
@@ -1,7 +1,19 @@
 function AdminMessages() {
   return (
-    <div style={{ padding: "50px" }}>
-      <h1>ADMIN PAGE WORKING ✅</h1>
+    <div style={{ padding: "100px", textAlign: "center" }}>
+      <h1 style={{ color: "red", fontSize: "40px" }}>
+        ADMIN PAGE WORKING ✅
+      </h1>
+    </div>
+  );
+}
+
+export default AdminMessages;function AdminMessages() {
+  return (
+    <div style={{ padding: "100px", textAlign: "center" }}>
+      <h1 style={{ color: "red", fontSize: "40px" }}>
+        ADMIN PAGE WORKING ✅
+      </h1>
     </div>
   );
 }
